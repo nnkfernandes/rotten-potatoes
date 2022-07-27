c0e014a64defd2c5bf531396af72774b1557c192
nnkfernandes <l1nd3c1s@gmail.com>
2022-07-27 16:07:33 -0300

    rotten potatoes skeleton

---
+++ .svelte-kit/runtime/env/static/private.js
@@ -0,0 +1,147 @@
+// this file is generated — do not edit it
+/** @type {import('$env/static/private').SPACESHIP_VERSION} */
+export const SPACESHIP_VERSION = "3.16.7";
+
+/** @type {import('$env/static/private').TERM_PROGRAM} */
+export const TERM_PROGRAM = "Apple_Terminal";
+
+/** @type {import('$env/static/private').NODE} */
+export const NODE = "/usr/local/bin/node";
+
+/** @type {import('$env/static/private').INIT_CWD} */
+export const INIT_CWD = "/Users/nico/Documents/projeto-final/svelte/rotten-potatoes/rotten-potatoes";
+
+/** @type {import('$env/static/private').SHELL} */
+export const SHELL = "/bin/zsh";
+
+/** @type {import('$env/static/private').TERM} */
+export const TERM = "xterm-256color";
+
+/** @type {import('$env/static/private').npm_config_metrics_registry} */
+export const npm_config_metrics_registry = "https://registry.npmjs.org/";
+
+/** @type {import('$env/static/private').TMPDIR} */
+export const TMPDIR = "/var/folders/mm/3b6tjhtx1ts82ykh1ktws1j40000gn/T/";
+
+/** @type {import('$env/static/private').npm_config_global_prefix} */
+export const npm_config_global_prefix = "/usr/local";
+
+/** @type {import('$env/static/private').TERM_PROGRAM_VERSION} */
+export const TERM_PROGRAM_VERSION = "445";
+
+/** @type {import('$env/static/private').COLOR} */
+export const COLOR = "1";
+
+/** @type {import('$env/static/private').TERM_SESSION_ID} */
+export const TERM_SESSION_ID = "9DFC4B13-B370-4E96-A71E-EC9868A35D55";
+
+/** @type {import('$env/static/private').npm_config_noproxy} */
+export const npm_config_noproxy = "";
+
+/** @type {import('$env/static/private').npm_config_local_prefix} */
+export const npm_config_local_prefix = "/Users/nico/Documents/projeto-final/svelte/rotten-potatoes/rotten-potatoes";
+
+/** @type {import('$env/static/private').ZSH} */
+export const ZSH = "/Users/nico/.oh-my-zsh";
+
+/** @type {import('$env/static/private').USER} */
+export const USER = "nico";
+
+/** @type {import('$env/static/private').npm_config_globalconfig} */
+export const npm_config_globalconfig = "/usr/local/etc/npmrc";
+
+/** @type {import('$env/static/private').SSH_AUTH_SOCK} */
+export const SSH_AUTH_SOCK = "/private/tmp/com.apple.launchd.7TrywyR77q/Listeners";
+
+/** @type {import('$env/static/private').__CF_USER_TEXT_ENCODING} */
+export const __CF_USER_TEXT_ENCODING = "0x1F5:0x0:0x0";
+
+/** @type {import('$env/static/private').npm_execpath} */
+export const npm_execpath = "/usr/local/lib/node_modules/npm/bin/npm-cli.js";
+
+/** @type {import('$env/static/private').PAGER} */
+export const PAGER = "less";
+
+/** @type {import('$env/static/private').LSCOLORS} */
+export const LSCOLORS = "Gxfxcxdxbxegedabagacad";
+
+/** @type {import('$env/static/private').PATH} */
+export const PATH = "/Users/nico/Documents/projeto-final/svelte/rotten-potatoes/rotten-potatoes/node_modules/.bin:/Users/nico/Documents/projeto-final/svelte/rotten-potatoes/node_modules/.bin:/Users/nico/Documents/projeto-final/svelte/node_modules/.bin:/Users/nico/Documents/projeto-final/node_modules/.bin:/Users/nico/Documents/node_modules/.bin:/Users/nico/node_modules/.bin:/Users/node_modules/.bin:/node_modules/.bin:/usr/local/lib/node_modules/npm/node_modules/@npmcli/run-script/lib/node-gyp-bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/Library/Apple/usr/bin";
+
+/** @type {import('$env/static/private').npm_package_json} */
+export const npm_package_json = "/Users/nico/Documents/projeto-final/svelte/rotten-potatoes/rotten-potatoes/package.json";
+
+/** @type {import('$env/static/private').npm_config_engine_strict} */
+export const npm_config_engine_strict = "true";
+
+/** @type {import('$env/static/private')._} */
+export const _ = "/Users/nico/Documents/projeto-final/svelte/rotten-potatoes/rotten-potatoes/node_modules/.bin/vite";
+
+/** @type {import('$env/static/private').npm_config_userconfig} */
+export const npm_config_userconfig = "/Users/nico/.npmrc";
+
+/** @type {import('$env/static/private').npm_config_init_module} */
+export const npm_config_init_module = "/Users/nico/.npm-init.js";
+
+/** @type {import('$env/static/private').__CFBundleIdentifier} */
+export const __CFBundleIdentifier = "com.apple.Terminal";
+
+/** @type {import('$env/static/private').npm_command} */
+export const npm_command = "run-script";
+
+/** @type {import('$env/static/private').PWD} */
+export const PWD = "/Users/nico/Documents/projeto-final/svelte/rotten-potatoes/rotten-potatoes";
+
+/** @type {import('$env/static/private').npm_lifecycle_event} */
+export const npm_lifecycle_event = "dev";
+
+/** @type {import('$env/static/private').EDITOR} */
+export const EDITOR = "vi";
+
+/** @type {import('$env/static/private').npm_package_name} */
+export const npm_package_name = "rotten-potatoes";
+
+/** @type {import('$env/static/private').XPC_FLAGS} */
+export const XPC_FLAGS = "0x0";
+
+/** @type {import('$env/static/private').npm_config_node_gyp} */
+export const npm_config_node_gyp = "/usr/local/lib/node_modules/npm/node_modules/node-gyp/bin/node-gyp.js";
+
+/** @type {import('$env/static/private').npm_package_version} */
+export const npm_package_version = "0.0.1";
+
+/** @type {import('$env/static/private').SPACESHIP_ROOT} */
+export const SPACESHIP_ROOT = "/Users/nico/.oh-my-zsh/custom/themes/spaceship-prompt";
+
+/** @type {import('$env/static/private').XPC_SERVICE_NAME} */
+export const XPC_SERVICE_NAME = "0";
+
+/** @type {import('$env/static/private').SHLVL} */
+export const SHLVL = "2";
+
+/** @type {import('$env/static/private').HOME} */
+export const HOME = "/Users/nico";
+
+/** @type {import('$env/static/private').npm_config_cache} */
+export const npm_config_cache = "/Users/nico/.npm";
+
+/** @type {import('$env/static/private').LESS} */
+export const LESS = "-R";
+
+/** @type {import('$env/static/private').LOGNAME} */
+export const LOGNAME = "nico";
+
+/** @type {import('$env/static/private').npm_lifecycle_script} */
+export const npm_lifecycle_script = "vite dev";
+
+/** @type {import('$env/static/private').LC_CTYPE} */
+export const LC_CTYPE = "UTF-8";
+
+/** @type {import('$env/static/private').npm_config_user_agent} */
+export const npm_config_user_agent = "npm/8.15.0 node/v16.16.0 darwin x64 workspaces/false";
+
+/** @type {import('$env/static/private').npm_node_execpath} */
+export const npm_node_execpath = "/usr/local/bin/node";
+
+/** @type {import('$env/static/private').npm_config_prefix} */
+export const npm_config_prefix = "/usr/local";
